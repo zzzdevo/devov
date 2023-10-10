d77a38882097f1d2bc8f6ab14a58c7ab7e610131
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-10 11:22:37 +0300

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -55,7 +55,7 @@ async def play_commnd(
     slider = None
     plist_type = None
     spotify = None
-    user_id = message.from_user.id if message.from_user else "833360381"
+    user_id = message.from_user.id if message.from_user else "3223679800"
     user_name = message.from_user.first_name if message.from_user else "None"
     audio_telegram = (
         (
